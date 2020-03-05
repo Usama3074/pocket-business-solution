f9c9b2ea4b73337f12831ca0595fa3d0a0d45824
safwanidrees <safwanidrees957@gmail.com>
2020-03-05 14:50:07 +0500

    Chart Bar

--- lib/screens/reports_screen.dart
+++ lib/screens/reports_screen.dart
@@ -1,38 +1,69 @@
 import 'package:flutter/material.dart';
+import '../values/values.dart';
+import '../screens/report//week.dart';
+import '../screens/report/month.dart';
+import '../screens/report/today.dart';
+class ReportsScreen extends StatefulWidget {
+  @override
+  _ReportsScreenState createState() => _ReportsScreenState();
+}
 
-class ReportsScreen extends StatelessWidget {
+class _ReportsScreenState extends State<ReportsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          InkWell(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Container(
-              width: 22,
-              height: 10,
-              margin: EdgeInsets.only(left: 30, top: 40),
-              child: Image.asset(
-                "assets/images/shape-3.png",
-                fit: BoxFit.none,
-              ),
-            ),
+    return DefaultTabController(
+      // initialIndex: 0,
+
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: AppColors.secondaryBackground,
+          title: Text('Salary of Employees'),
+          bottom: TabBar(
+
+              labelColor: Colors.redAccent,
+              unselectedLabelColor: Colors.white,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicator: BoxDecoration(
+
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10)),
+                  color: Colors.white),
+
+              indicatorColor: Colors.white,
+//             labelColor: AppColors.accentElement,
+
+
+              // labelStyle: TextStyle(color: Colors.black),
+              tabs: <Widget> [
+                Tab(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text("TODAY"),
+                  ),
+                ),
+                Tab(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text("WEEK"),
+                  ),
+                ),
+                Tab(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text("MONTH"),
+                  ),
+                ),
+              ]
           ),
-          Expanded(
-            child: Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.only(top: 10),
-              child: Image.asset(
-                'assets/images/report_screen.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-          )
-        ],
+        ),
+        body:TabBarView(children: <Widget>[
+          Today(),
+          Week(),
+          Month(),
+        ],),
       ),
     );
   }
--- lib/screens/sales_sheet.dart
+++ lib/screens/sales_sheet.dart
@@ -1,79 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_bussiness_solution/main.dart';
 import '../values/values.dart';
+import '../screens/salary/week.dart';
+import '../screens/salary/month.dart';
+import '../screens/salary/today.dart';
 
-class SalesSheet extends StatelessWidget {
+class SalesSheet extends StatefulWidget {
+  @override
+  _SalesSheetState createState() => _SalesSheetState();
+}
+
+class _SalesSheetState extends State<SalesSheet> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        constraints: BoxConstraints.expand(),
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 255, 255, 255),
-        ),
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 0),
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            color: AppColors.secondaryBackground,
-            // border: Border.fromBorderSide(Borders.primaryBorder),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 40, bottom: 10,left: 20,right:20),
-                width: MediaQuery.of(context).size.width,
-                height: 35,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: Container(
-                          width: 22,
-                          height: 10,
-                          margin: EdgeInsets.only(top: 14),
-                          child: Image.asset(
-                            "assets/images/shape-4.png",
-                            fit: BoxFit.none,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: Container(
-                        margin: EdgeInsets.only(left: 70,top:5),
-                        child: Text(
-                          "Salary of Employees",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 249, 249, 249),
-                            fontWeight: FontWeight.w400,
-                            fontSize: 23,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+
+    return DefaultTabController(
+      // initialIndex: 0,
+
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: AppColors.secondaryBackground,
+          title: Text('Salary of Employees'),
+          bottom: TabBar(
+
+            labelColor: Colors.redAccent,
+            unselectedLabelColor: Colors.white,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicator: BoxDecoration(
+
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10)),
+                color: Colors.white),
+
+             indicatorColor: Colors.white,
+//             labelColor: AppColors.accentElement,
+
+
+            // labelStyle: TextStyle(color: Colors.black),
+            tabs: <Widget> [
+              Tab(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text("TODAY"),
                 ),
               ),
-              Expanded(
-                child: Container(
-                  child: Image.asset(
-                    'assets/images/sales_sheet.png',
-                    fit: BoxFit.fill,
-                  ),
+              Tab(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text("WEEK"),
                 ),
-              )
-            ],
+              ),
+              Tab(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text("MONTH"),
+                ),
+              ),
+            ]
           ),
         ),
+        body:TabBarView(children: <Widget>[
+          Today(),
+          Week(),
+          Month(),
+        ],),
       ),
     );
+//    return Scaffold(
+//        appBar: AppBar(title: Text('Salary of Employees'),backgroundColor: AppColors.secondaryBackground,centerTitle: true,),
+//        body: _UpperTabBar(),
+
+
+//      body: Container(
+//        constraints: BoxConstraints.expand(),
+//        decoration: BoxDecoration(
+//          color: Color.fromARGB(255, 255, 255, 255),
+//        ),
+//        child: Container(
+//          padding: EdgeInsets.symmetric(horizontal: 0),
+//          height: MediaQuery.of(context).size.height,
+//          decoration: BoxDecoration(
+//            color: AppColors.secondaryBackground,
+//            // border: Border.fromBorderSide(Borders.primaryBorder),
+//          ),
+//          child: Column(
+//            crossAxisAlignment: CrossAxisAlignment.stretch,
+//            children: [
+//              Container(
+//                margin: EdgeInsets.only(top: 40, bottom: 10,left: 20,right:20),
+//                width: MediaQuery.of(context).size.width,
+//                height: 35,
+//                child: Row(
+//                  crossAxisAlignment: CrossAxisAlignment.stretch,
+//                  children: [
+//                    Align(
+//                      alignment: Alignment.topLeft,
+//                      child: InkWell(
+//                        onTap: () {
+//                          Navigator.pop(context);
+//                        },
+//                        child: Container(
+//                          width: 22,
+//                          height: 10,
+//                          margin: EdgeInsets.only(top: 14),
+//                          child: Image.asset(
+//                            "assets/images/shape-4.png",
+//                            fit: BoxFit.none,
+//                          ),
+//                        ),
+//                      ),
+//                    ),
+//                    Align(
+//                      alignment: Alignment.topCenter,
+//                      child: Container(
+//                        margin: EdgeInsets.only(left: 70,top:5),
+//                        child: Text(
+//                          "Salary of Employees",
+//                          textAlign: TextAlign.center,
+//                          style: TextStyle(
+//                            color: Color.fromARGB(255, 249, 249, 249),
+//                            fontWeight: FontWeight.w400,
+//                            fontSize: 23,
+//                          ),
+//                        ),
+//                      ),
+//                    ),
+//                  ],
+//                ),
+//              ),
+//              Expanded(
+//                child: Container(
+//                  child: Image.asset(
+//                    'assets/images/sales_sheet.png',
+//                    fit: BoxFit.fill,
+//                  ),
+//                ),
+//              )
+//            ],
+//          ),
+//        ),
+//      ),
+//    );
   }
 }
